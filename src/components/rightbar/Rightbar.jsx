ddc414371d61a497f90b59db4439ddcc805d9d9a
Sarthak Vats <80093553+sarthakvats-2002@users.noreply.github.com>
2023-09-10 23:52:50 +0530

    Update Rightbar.jsx for follow unfollow

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -12,7 +12,7 @@ export default function Rightbar({ user }) {
   const [friends, setFriends] = useState([]);
   const { user: currentUser, dispatch } = useContext(AuthContext);
   const [followed, setFollowed] = useState(
-    currentUser.followings.includes(user?.id)
+    !currentUser.followings.includes(user?.id)
   );
   const baseUrl = "https://socialappbackend-2wht.onrender.com/api"
 
@@ -33,15 +33,15 @@ export default function Rightbar({ user }) {
   const handleClick = async () => {
     try {
       if (followed) {
-        await axios.put(baseUrl +`/users/${user._id}/unfollow`, {
+        await axios.put(baseUrl +`/users/${user?._id}/unfollow`, {
           userId: currentUser._id,
         });
-        dispatch({ type: "UNFOLLOW", payload: user._id });
+        dispatch({ type: "UNFOLLOW", payload: user?._id });
       } else {
-        await axios.put(baseUrl+`/users/${user._id}/follow`, {
+        await axios.put(baseUrl+`/users/${user?._id}/follow`, {
           userId: currentUser._id,
         });
-        dispatch({ type: "FOLLOW", payload: user._id });
+        dispatch({ type: "FOLLOW", payload: user?._id });
       }
       setFollowed(!followed);
     } catch (err) {
